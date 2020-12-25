522e288f6e3685a0b2c798738dd193fec073f052
hercules-sousa <herculesdeso@gmail.com>
2020-12-25 15:05:20 -0300

    feat: adding image

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import StandardButton from './components/ButtonStandard';
+import backgroundImage from './assets/backgroundImage.png'
 
 function App() {
   return (
     <div className="container">
+      <img id="nailsImage" src={backgroundImage} alt="Nails Design"/>
       <StandardButton label="Contatos" />
     </div>
   );
