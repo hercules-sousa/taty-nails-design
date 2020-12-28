bf37d6c1224f1843c138629e18215e575cc60945
hercules-sousa <herculesdeso@gmail.com>
2020-12-28 15:20:30 -0300

    style: putting background in place

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
 import StandardButton from './components/ButtonStandard';
-import backgroundImage from './assets/backgroundImage.png'
 
 function App() {
   return (
     <div className="container">
-      <img id="nailsImage" src={backgroundImage} alt="Nails Design"/>
       <StandardButton label="Contatos" />
     </div>
   );
