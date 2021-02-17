7ec5c26e12766262edc8a424b9e8529e7d9bc85e
hercules-sousa <herculesdeso@gmail.com>
2021-02-17 15:28:17 -0300

    style: adding more images

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import "./App.css";
 
-import logo from "./assets/images/logoMarca.png";
-
 function App() {
   return (
-    <div className="container">
-      <header>
-        <h3>Home</h3>
-        <h3 className="space-given">Galeria</h3>
-        <h3 className="space-given">Contato</h3>
-      </header>
+    <>
+      <div id="first-screen">
+        <header>
+          <h3>Home</h3>
+          <h3 className="space-given">Galeria</h3>
+          <h3 className="space-given">Contato</h3>
+        </header>
 
-      <main>
-        <div id="logo-container" />
-      </main>
-    </div>
+        <main>
+          <div id="logo-container" />
+        </main>
+      </div>
+      <div id="info-container"></div>
+      <div id="nails-cards-container"></div>
+    </>
   );
 }
 
