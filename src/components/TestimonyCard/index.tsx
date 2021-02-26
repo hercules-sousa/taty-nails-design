e170b3fe3a3d4ae1bafab6e47c68a8075d2f54a3
hercules-sousa <herculesdeso@gmail.com>
2021-02-26 10:38:46 -0300

    feat: adding icons

--- src/components/TestimonyCard/index.tsx
+++ src/components/TestimonyCard/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaInstagram } from "react-icons/fa";
+import { RiFacebookCircleLine } from "react-icons/ri";
 
 import "./styles.css";
 
@@ -7,6 +9,14 @@ const TestimonyCard = () => {
     <div id="testimony-card-container">
       <div id="background-sheet">
         <div id="image-sheet" />
+        <div className="card-box-one"></div>
+        <div className="card-box-two">
+          <div className="testimony-card-image-container" />
+          <div className="testimony-card-icons-container">
+            <RiFacebookCircleLine size={40} color="white" />
+            <FaInstagram size={40} color="white" />
+          </div>
+        </div>
       </div>
     </div>
   );
